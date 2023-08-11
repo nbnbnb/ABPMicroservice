3c4f14e6ed5743b748bd3d78bdaf83e2e5ac36fe
nbnbnb <nbnbnb.zhang@gmail.com>
2023-08-11 23:06:19 +0800

    Update seoul-arm-deploy-single-by-hook.sh

--- seoul-arm-deploy-single-by-hook.sh
+++ seoul-arm-deploy-single-by-hook.sh
@@ -7,5 +7,10 @@ git checkout main
 git pull
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:$2
-docker compose -f aliyun-docker-compose.yml --env-file .env.arm up -d $1
 
+# docker compose -f aliyun-docker-compose.yml --env-file .env.arm up -d $1
+
+# Clean & Up
+docker compose -f aliyun-docker-compose.yml --env-file .env.arm stop $1
+docker compose -f aliyun-docker-compose.yml --env-file .env.arm rm -f $1
+docker compose -f aliyun-docker-compose.yml --env-file .env.arm up -d $1
